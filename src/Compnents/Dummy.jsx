3843d7f651c9b4f37311770302e35295d30db11a
MUIZ OWOLABI <beasteye.official@gmail.com>
2023-12-28 11:45:19 +0100

    testing

--- src/Compnents/Dummy.jsx
+++ src/Compnents/Dummy.jsx
@@ -1,5 +1,5 @@
 import Dashboard from "../assets/MoneyDash.svg";
-import profile from "../assets/Profile.svg";
+
 import beggars from "../assets/Beggers.svg";
 import history from "../assets/Transaction.svg";
 
